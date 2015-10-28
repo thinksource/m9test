d324daf2c26245555b6576f1cd6f48ed0fbf91f7
foretribe <foretribe@gmail.com>
2015-10-29 09:59:21 +1100

    test fold  add

---
+++ test/test.js
@@ -0,0 +1,10 @@
+var mocha=require('mocha');
+var test = require('unit.js');
+var chai=require('chai');
+var expect=require('chai').expect;
+
+describe("json", function(){
+  it("return lowercase of a string", function(){
+
+  })
+})
