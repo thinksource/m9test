771d4f46ac28904b89a8a0c9b807b28fa0bb1f57
thinksource <foretribe@gmail.com>
2016-12-19 21:56:18 +1100

    change test.js

--- test/test.js
+++ test/test.js
@@ -10,7 +10,8 @@ chai.use(chaiHttp);
 
 describe("/ POST json", function(){
   var url="http://localhost:8000/";
-  var example_req=parse(String(fs.readFileSync("data/request.json")));
+  var example_req=String(fs.readFileSync("data/request.json"));
+  var error_req=String(fs.readFileSync("data/error.json"));
   var example_res=JSON.parse(String(fs.readFileSync("data/response.json")));
   it("example output", ()=>{
 
@@ -23,11 +24,31 @@ describe("/ POST json", function(){
           res.body.should.have.property('response');
           res.body.response.should.be.a('array');
           res.body.response.length.be.eql(7);
-          //res.body.should.be.eql(exmple_res);
+          res.body.should.be.eql(exmple_res);
           done();
-        });
+        })
   });
-  // it("error"
-  //
-  // );
+  it("error response", ()=>{
+    chai.request(server)
+        .post('/')
+        .send(error_req)
+        .end((err, res)=>{
+          res.should.have.status(400);
+          res.body.should.be.a('object');
+          res.body.should.have.property('error');
+          res.body.error.should.be.eql("Could not decode request: JSON parsing failed");
+          done();
+        })
+  });
+  it("Get method is invalid",()=>{
+    chai.request(server)
+      .get('/')
+      .end((err, res)=>{
+        res.should.have.status(400);
+        res.body.should.be.a('object');
+        res.body.should.have.property('error');
+        res.body.error.should.be.eql("Only post method accept for this service");
+        done();
+      })
+  })
 })
